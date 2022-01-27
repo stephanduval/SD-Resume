8661b417803be3118e1e84a515dbdc753058c4a2
Stephan DuVal <stephan.duval@gmail.com>
2022-01-27 05:25:20 +0200

    Working on lists for the technical section

--- src/Resume-Page/Resume-Page-Functions.js
+++ src/Resume-Page/Resume-Page-Functions.js
@@ -42,51 +42,54 @@ export const parseSpeficiedDataFRomResumeJSON = (resumeData,tier1,tier2,tier3)=>
 }
 
 
-export const populateWorkSection = (objFromJSON,title) => {
+export const populateWorkSection = (objFromJSON, title) => {
   // const work = objFromJSON['work'];
-    const work = "basics" // how can I destructure the object call????
+    const work = "work" 
+    const startDate = "startDate";
+    const endDate = "endDate";
     const populateSection = (iterationNumber) => {
 
     
-    const workContainer = document.getElementById("work-container");
+    const sectionContainer = document.getElementById("work-container");
         
-        let JSONString = "Title";  //Unique Value
+        let JSONString = title;  //Unique Value
         let div = document.createElement("div");
         let paragraph = document.createElement("p");
         let text = document.createTextNode(JSONString); 
-        workContainer.appendChild(div); 
+        sectionContainer.appendChild(div); 
         div.appendChild(paragraph);
         paragraph.appendChild(text);
-        div.classList.add("work-title");  // Unique Value
+        div.classList.add("heading-title");  // Unique Value
         div.setAttribute("id", "work-title");  // Unique Value
 
 
 
         // TEXT Field:
-        JSONString = JSON.stringify(objFromJSON.work[iterationNumber].name);
+        JSONString = objFromJSON[work][iterationNumber].name;
         div = document.createElement("div");
         paragraph = document.createElement("p");
         text = document.createTextNode(JSONString); 
-        workContainer.appendChild(div); 
+        sectionContainer.appendChild(div); 
         div.appendChild(paragraph);
         paragraph.appendChild(text);
-        div.classList.add("work-text");  // Unique Value
+        div.classList.add("row-two-to-five-text-field");  // Unique Value
         div.setAttribute("id", "work-text");  // Unique Value
         
 
 
         // DATE FIELD:
-        JSONString = "THE DATE" 
+        JSONString = JSONString = objFromJSON[work][iterationNumber][startDate]+ "-" 
+        + objFromJSON[work][iterationNumber][endDate];
+
         div = document.createElement("div");
         paragraph = document.createElement("p");
         text = document.createTextNode(JSONString); 
-        workContainer.appendChild(div); 
+        sectionContainer.appendChild(div); 
         div.appendChild(paragraph);
         paragraph.appendChild(text);            
-        div.classList.add("work-date");
+        div.classList.add("date");
         div.setAttribute("id", "work-text")
-        // workDateHeader.element.setAttribute("class", "work-date")
-        // workTitleEntry.classList.add('work-date');   
+  
  }
 
   for (let iterationNumber = 0; iterationNumber < Object.keys(objFromJSON.work).length; iterationNumber++) {
@@ -97,8 +100,139 @@ export const populateWorkSection = (objFromJSON,title) => {
   }
 }
 
-export const populateEducationSection = () => {
+export const populateTechnicalSection = (objFromJSON,title) => {
+ // const work = objFromJSON['work'];
+  const projects = "projects";
+  const keywords = "keywords"
 
+  const populateSection = (iterationNumber) => {
+
+ const sectionContainer = document.getElementById("technical-container");
+     
+     let JSONString = title;  //Unique Value
+     let div = document.createElement("div");
+     let paragraph = document.createElement("p");
+     let text = document.createTextNode(JSONString); 
+     
+     sectionContainer.appendChild(div); 
+     div.appendChild(paragraph);
+     paragraph.appendChild(text);
+     div.classList.add("heading-title");  // Unique Value
+     div.setAttribute("id", "work-title");  // Unique Value
+
+
+     // List 
+
+     let unorderedList = document.createElement("ul");
+      let listItem = document.createElement("li");
+    
+    div = document.createElement("div");
+
+    sectionContainer.appendChild(div); 
+    // div.classList.add("row-two-to-five-text-field");  // Unique Value
+
+     div.appendChild(unorderedList);
+     
+     for (let technicalListIterationNumber = 0; iterationNumber < 20; iterationNumber++) {
+      listItem = document.createElement("li")
+      unorderedList.appendChild(listItem);
+      listItem.appendChild(document.createTextNode("list"));  
+      if (iterationNumber % 3 == 0) {
+        div = document.createElement("div");
+
+        sectionContainer.appendChild(div); 
+
+        div.appendChild(unorderedList);
+
+
+      }
+     }
+    //  populateList();
+     
+  //    let keywordIterationNumber = 0;
+  //    for (let technicalListIterationNumber = 0; iterationNumber < Object.keys(objFromJSON[projects]).length; iterationNumber++) {
+    
+  //    JSONString = "test"
+  //    //objFromJSON[projects][keywords][keywordIterationNumber]
+  //    unorderedList.appendChild(listItem);
+  //    unorderedList.appendChild(listItem);
+
+  //    unorderedList.appendChild(listItem);
+
+  //    listItem.appendChild(text);
+  //    keywordIterationNumber++
+  //   }
+  //    div.classList.add("row-two-to-six-text-field");  // Unique Value
+  //    div.setAttribute("id", "work-title");  // Unique Value
+
+
+
+}
+
+// for (let iterationNumber = 0; iterationNumber < Object.keys(objFromJSON.work).length; iterationNumber++) {
+// // div.className = 'work';  The code to assign a className isn't working
+
+populateSection(5);
+
+// }
+}
+
+export const populateEducationSection = (objFromJSON, title) => {
+  
+  const education = "education"
+  const startDate = "startDate";
+  const endDate = "endDate";
+  const populateSection = (iterationNumber) => {
+
+
+  
+  const sectionContainer = document.getElementById("education-container");
+      
+      let JSONString = title;  //Unique Value
+      let div = document.createElement("div");
+      let paragraph = document.createElement("p");
+      let text = document.createTextNode(JSONString); 
+      sectionContainer.appendChild(div); 
+      div.appendChild(paragraph);
+      paragraph.appendChild(text);
+      div.classList.add("heading-title");  // Unique Value
+      div.setAttribute("id", "education-title");  // Unique Value
+
+
+
+      // TEXT Field:
+      JSONString = objFromJSON.education[iterationNumber].institution
+      div = document.createElement("div");
+      paragraph = document.createElement("p");
+      text = document.createTextNode(JSONString); 
+      sectionContainer.appendChild(div); 
+      div.appendChild(paragraph);
+      paragraph.appendChild(text);
+     div.classList.add("row-two-to-five-text-field");  // Unique Value
+      div.setAttribute("id", "education-text");  // Unique Value
+      
+
+
+      // DATE FIELD:
+      JSONString = JSONString = objFromJSON[education][iterationNumber][startDate] + "-" 
+      + objFromJSON[education][iterationNumber][endDate];
+      div = document.createElement("div");
+      paragraph = document.createElement("p");
+      text = document.createTextNode(JSONString); 
+      sectionContainer.appendChild(div); 
+      div.appendChild(paragraph);
+      paragraph.appendChild(text);            
+     div.classList.add("date");
+      div.setAttribute("id", "education-text")
+
+}
+
+for (let iterationNumber = 0; iterationNumber < Object.keys(objFromJSON.education).length; iterationNumber++) {
+// div.className = 'work';  The code to assign a className isn't working
+
+populateSection(iterationNumber);
+
+}
 
 }
 
--- src/Resume-Page/Resume-Page.js
+++ src/Resume-Page/Resume-Page.js
@@ -1,70 +1,63 @@
 import resumeData from './Resume-data.json';
 import { useEffect } from 'react';
 import './Resume-Page.css';
-import { showWork, populateWorkSection } from './Resume-Page-Functions';
+import { populateEducationSection, populateWorkSection, populateTechnicalSection } from './Resume-Page-Functions';
 
-let basics ="basics";
-let name = "name";
-
-let param = "basics.name";
-// const resumeDataParsed = JSON.parse(resumeData);
-const resumeDataString = JSON.stringify(resumeData.work);
-
-
-console.log("resumeDataString",resumeDataString)
-console.log("JSON",resumeData.work)
-// console.log("JSON LENGTH",object.keys(resumeData).length())
-// console.log("resumeDataParsed",resumeDataParsed)
-// }
 
 
 function ResumeActual() {
 
 
   useEffect(() => {
-    populateWorkSection(resumeData);  });
+    populateWorkSection(resumeData, "Experience");
+    populateEducationSection(resumeData, "Education");  
+    populateTechnicalSection(resumeData, "Technical Skills")
+  });
 
     return (
       <div className="ResumePage">
       <div className="grid-container">
-        <div className="resume-header">Stephan Douglas DuVal
-        <div>Full Stack Developer</div>
+        <div className="resume-header">Name Name Name
+        <div>Job Job Job</div>
         </div>
         <div className="contact-information">Download
         <div>778-788-3800</div>
         <div>stephan.duval@gmail.com</div>
         </div>
-        <div className="row-title">profile</div>
+        <div className="heading-title">profile</div>
         <div className="profile">A profile card</div>
-        <div className="row-title">Skills</div>
+        <div className="heading-title">Skills</div>
         <div className="skills">Skill1</div>
         <div className="skills">Skill2</div>
         <div className="skills">Skill3</div>
         <div className="skills">Skill3</div>
+      
 
-        <div className="row-title">Technical</div>
-        <div className="technical">A technical card</div>
-        <div className="workDiv">
+        <div className="section-div"> 
         
-            <div id="work-container" className="work-container">
-              <div id="work-title" className="work-title">DEFAULT Title</div>
-              <div id="work-text" className="work-text">default text</div>
-              <div id="work-date" className="work-date">default date</div>
+        <div id="technical-container" className="five-column-grid-container">
+        <div>SDS</div>
+        <div>SDSSD</div>
+        <div>SDS</div>
+        <div>SDSSD</div>
+        <div> LAST</div>
+
+        </div>
+    </div>
+
+        <div className="section-div"> 
+        
+            <div id="work-container" className="five-column-grid-container">
+
              
             </div>
         </div>
         
-
-
-
-        <div className="row-title">Education</div>
-        <div className="education-card">An education card</div>
-
       
-        <div className ="workDiv">
-          <div id="work-container" className="education-container">
-            <div className="row-title">Education</div>
-            <div className="education-card">An education card</div>
+        <div className ="section-div">
+          <div id="education-container" className="five-column-grid-container">
+         
+
           </div>
           
       </div>
