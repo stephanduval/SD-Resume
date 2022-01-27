c014180d76aa07998c2e36361bc04962c1cf3e00
Stephan DuVal <stephan.duval@gmail.com>
2022-01-27 15:10:20 +0000

    got the skills and work loops working in better order

---
+++ src/Resume-Page/Experience-Component.js
@@ -0,0 +1,19 @@
+import { constants } from './Resume-Page-Contants';
+
+
+
+function ExperienceComponent() {
+    return (
+      <div id="ExperinceComponent" className="five-column-grid-container">
+        <div className="heading-title">{constants.ExperienceHeading}</div>
+        <div className="row-two-to-six-text-field">
+
+            <div id="SkillsFlexContainer" className="five-column-flex-container">
+            
+            </div>
+          </div>
+      </div>
+    );
+  }
+
+  export default ExperienceComponent
